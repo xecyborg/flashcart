01c6e55411cc102b6797a9d61e0c5b37cd47d17b
Maddy <zyfroxx@gmail.com>
2024-10-26 10:28:45 +0530

    added file serializers and urls in store

--- flashcart/urls.py
+++ flashcart/urls.py
@@ -23,5 +23,7 @@ admin.site.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('testroom/', include('testroom.urls')),
+    path('store/', include('store.urls')),
     path('__debug__/', include(debug_toolbar.urls))
 ]
