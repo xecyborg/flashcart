1ce6d444773b07b01999d18b2b5c4976a069feaf
Maddy <zyfroxx@gmail.com>
2024-10-27 10:15:32 +0530

    changed urls to use routers

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.urls.conf import include
+from rest_framework_nested import routers
 from . import views
 
-urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail),
-    path('collections/', views.collection_list),
-    path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
-]
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='products')
+router.register('collections', views.CollectionViewSet)
+
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+
+urlpatterns = router.urls + products_router.urls
