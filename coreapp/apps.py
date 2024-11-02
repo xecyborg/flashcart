b45ee053a8fc6dc37df00d4f0cf27df92b7ed1f9
Maddy <zyfroxx@gmail.com>
2024-11-02 10:53:52 +0530

    added signals for coreapp

--- coreapp/apps.py
+++ coreapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CoreappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coreapp'
+
+    def ready(self) -> None:
+        import coreapp.signals.handlers
