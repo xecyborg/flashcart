8fe83a16153c6600ace63c2d50f8eb42f86a63a0
Maddy <zyfroxx@gmail.com>
2024-11-02 10:15:57 +0530

    Added permissions for order authentication

--- store/views.py
+++ store/views.py
@@ -113,6 +113,11 @@ class CustomerViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
 
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
     def get_queryset(self):
         user = self.request.user
 
